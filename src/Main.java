f486062bbf64de9a53631cfa6922ff8e2868ef68
IlyaMahnach <ilya_mahnach@mail.ru>
2022-02-09 20:03:56 +0700

    Коммит

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        // Данные
+        int refill = 1000;
+        int balance = 2000;
+        //Логика
+        int bonus = (refill >= 1000) ? refill / 100 : 0;
+        int total = (refill + balance + bonus);
+
+        System.out.println("Начислен бонус " + bonus);
+        System.out.println("Итоговый баланс " + total);
+    }}
